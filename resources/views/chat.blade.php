f1a3f439e629724d01b867608ac139629a53b43e
natsume0718 <moto718program@gmail.com>
2019-07-03 17:17:46 +0900

    [update] baseレイアウト作成

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,13 +9,22 @@
     <title>Chat</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
+
 <body>
     <div class="container">
         <div class="row" id="app">
-            <h1>Chat Room</h1>
+            <ul class="list-group offset-4 col-4">
+                <li class="list-group-item active">Chat Room</li>
+                <li class="list-group-item">Dapibus ac facilisis in</li>
+                <li class="list-group-item">Morbi leo risus</li>
+                <li class="list-group-item">Porta ac consectetur ac</li>
+                <li class="list-group-item">Vestibulum at eros</li>
+                <input type="text" class="form-controll" placeholder="メッセージ">
+            </ul>
         </div>
     </div>
-    
+
     <script src="{{ asset('js/app.js')}}"></script>
 </body>
+
 </html>
