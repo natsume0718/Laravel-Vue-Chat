17cc8675f5491e0c85f22c5ce0438d3a108d8a7b
natsume0718 <moto718program@gmail.com>
2019-07-04 18:18:41 +0900

    [update] AuthのルーティングをADR対応に変更

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,25 @@
 |
 */
 
-Route::get('/', \App\Http\Actions\ChatSendAction::class);
+Route::get('/send', \App\Http\Actions\ChatSendAction::class);
+Route::get('/', function () {
+    return view('chat');
+});
+Route::get('/home', function () {
+    return view('home');
+});
 
-Auth::routes();
+// Authentication Routes...
+Route::get('login', '\App\Http\Controllers\Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', '\App\Http\Controllers\Auth\LoginController@login');
+Route::post('logout', '\App\Http\Controllers\Auth\LoginController@logout')->name('logout');
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Registration Routes...
+Route::get('register', '\App\Http\Controllers\Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', '\App\Http\Controllers\Auth\RegisterController@register');
+
+// Password Reset Routes...
+Route::get('password/reset', '\App\Http\Controllers\Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', '\App\Http\Controllers\Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', '\App\Http\Controllers\Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', '\App\Http\Controllers\Auth\ResetPasswordController@reset');
